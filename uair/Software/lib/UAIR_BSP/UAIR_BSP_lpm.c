f70bb3ab5cf6c40223e8668426e0a979ba07f4d5
Alvaro <alvieboy@alvie.com>
2021-11-20 21:35:38 +0000

    lpm: add debug pin

--- uair/Software/lib/UAIR_BSP/UAIR_BSP_lpm.c
+++ uair/Software/lib/UAIR_BSP/UAIR_BSP_lpm.c
@@ -9,9 +9,12 @@ void UAIR_LPM_PostStopModeHook()
     HAL_clk_resume_clocks();
 
     TRACER_RESUME();
+    UAIR_BSP_DP_Off(DEBUG_PIN2);
+
 }
 
 void BSP_LPM_enter_low_power_mode()
 {
+    UAIR_BSP_DP_On(DEBUG_PIN2);
     UAIR_LPM_EnterLowPower();
 }
