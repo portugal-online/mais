c0c99be9f6ddc8f2074f7e7a5c03ef6bdbd1962d
Alvaro <alvieboy@alvie.com>
2021-11-20 21:37:36 +0000

    OAQ2: add ZMOD4510 API routines and AQI calculation

--- uair/Software/lib/ZMOD4510/ZMOD4510.c
+++ uair/Software/lib/ZMOD4510/ZMOD4510.c
@@ -1,17 +1,40 @@
 #include "ZMOD4510.h"
 #include <string.h>
+#include "BSP.h"
+
+#include "zmod4xxx_api.h"
 
 #define ZMOD_DEFAULT_I2C_ADDRESS (0x33)
 #define ZMOD_DEFAULT_I2C_TIMEOUT (500)
 
-struct ZMOD4510 {
-    HAL_I2C_bus_t bus;
-    uint8_t address;
-    unsigned i2c_timeout;
-    HAL_GPIO_t reset_gpio;
-};
+#define MAX_ZMOD_SENSORS 1
 
-typedef struct ZMOD4510 ZMOD4510_t;
+
+
+/* Registry */
+static ZMOD4510_t *zmod_registry[MAX_ZMOD_SENSORS];
+static uint8_t zmod_registry_idx = 0;
+
+static int8_t ZMOD4510_register(ZMOD4510_t *zmod)
+{
+    int8_t ret = -1;
+    if (zmod_registry_idx<MAX_ZMOD_SENSORS) {
+        zmod_registry[zmod_registry_idx] = zmod;
+        ret = zmod_registry_idx;
+        zmod_registry_idx++;
+    }
+    return ret;
+}
+
+static ZMOD4510_t *ZMOD4510_registry_get(uint8_t index)
+{
+    if (zmod_registry_idx==0)
+        return NULL;
+
+    if (index>(zmod_registry_idx-1))
+        return NULL;
+    return zmod_registry[index];
+}
 
 static HAL_StatusTypeDef ZMOD4510_i2c_read(ZMOD4510_t *zmod, uint8_t startreg, uint8_t *data, uint16_t count)
 {
@@ -47,6 +70,43 @@ static ZMOD4510_op_result_t ZMOD4510_Reset(ZMOD4510_t *zmod)
     return ZMOD4510_OP_SUCCESS;
 }
 
+static int8_t ZMOD4510_i2c_read_api_wrapper(uint8_t i2c_address, uint8_t reg_addr,
+                                            uint8_t *data_buf, uint8_t len)
+{
+    ZMOD4510_t *self = ZMOD4510_registry_get(i2c_address);
+    if (NULL==self) {
+        BSP_TRACE("Invalid read to ZMOD4510 device @%d",i2c_address);
+        return -1;
+    }
+
+    ZMOD4510_op_result_t r = ZMOD4510_i2c_read(self, reg_addr, data_buf, len);
+    if (r==ZMOD4510_OP_SUCCESS)
+        return 0;
+    else
+        return -1;
+}
+
+static int8_t ZMOD4510_i2c_write_api_wrapper(uint8_t i2c_address, uint8_t reg_addr,
+                                             uint8_t *data_buf, uint8_t len)
+{
+    ZMOD4510_t *self = ZMOD4510_registry_get(i2c_address);
+    if (NULL==self) {
+        BSP_TRACE("Invalid read to ZMOD4510 device @%d",i2c_address);
+        return -1;
+    }
+    ZMOD4510_op_result_t r = ZMOD4510_i2c_write(self, reg_addr, data_buf, len);
+    if (r==ZMOD4510_OP_SUCCESS)
+        return 0;
+    else
+        return -1;
+}
+
+
+static void ZMOD4510_delay_ms(uint32_t ms)
+{
+    // TBD
+    HAL_delay_us(ms * 1000);
+}
 
 ZMOD4510_op_result_t ZMOD4510_Init(ZMOD4510_t *zmod, HAL_I2C_bus_t bus, HAL_GPIO_t reset_gpio)
 {
@@ -59,7 +119,31 @@ ZMOD4510_op_result_t ZMOD4510_Init(ZMOD4510_t *zmod, HAL_I2C_bus_t bus, HAL_GPIO
         HAL_GPIO_configure_output_od(reset_gpio);
     }
 
+    zmod->dev.pid = ZMOD4510_PID;
+    zmod->dev.init_conf = &zmod_oaq_sensor_type[Z_INIT];
+    zmod->dev.meas_conf = &zmod_oaq_sensor_type[Z_MEASURE];
+    zmod->dev.prod_data = zmod->prod_data;
+
+    zmod->dev.read  = ZMOD4510_i2c_read_api_wrapper;
+    zmod->dev.write = ZMOD4510_i2c_write_api_wrapper;
+
+    zmod->dev.delay_ms = ZMOD4510_delay_ms;
+    zmod->dev.pvt = zmod; // Point back to us
+
+    int8_t pseudo_id = ZMOD4510_register(zmod);
+
+    if (pseudo_id <0)
+        return ZMOD4510_OP_INTERNAL_ERROR;
+
+    zmod->dev.i2c_addr = pseudo_id;
+
+
     return ZMOD4510_OP_SUCCESS;
+}
+
+zmod4xxx_dev_t *ZMOD4510_get_dev(ZMOD4510_t *zmod)
+{
+    return &zmod->dev;
 }
 
 ZMOD4510_op_result_t ZMOD4510_Probe(ZMOD4510_t *zmod)
@@ -101,6 +185,71 @@ ZMOD4510_op_result_t ZMOD4510_Probe(ZMOD4510_t *zmod)
             return ZMOD4510_OP_DEVICE_ERROR;
         }
     }
+
+    int8_t api_ret = zmod4xxx_read_sensor_info(&zmod->dev);
+    if (api_ret|=0) {
+        BSP_TRACE("Cannot read sensor info");
+        return ZMOD4510_OP_DEVICE_ERROR;
+    }
+
+    api_ret = zmod4xxx_prepare_sensor(&zmod->dev);
+    if (api_ret!=0) {
+        BSP_TRACE("Cannot prepare sensor");
+        return ZMOD4510_OP_DEVICE_ERROR;
+    }
+
     return ZMOD4510_OP_SUCCESS;
 }
+
+ZMOD4510_op_result_t ZMOD4510_start_measurement(ZMOD4510_t *zmod)
+{
+    int8_t api_ret = zmod4xxx_start_measurement(&zmod->dev);
+    if (api_ret==0) {
+        return ZMOD4510_OP_SUCCESS;
+    }
+    return ZMOD4510_OP_DEVICE_ERROR;
+}
+
+ZMOD4510_op_result_t ZMOD4510_read_adc(ZMOD4510_t *zmod)
+{
+    int8_t api_ret = zmod4xxx_read_adc_result(&zmod->dev, zmod->adc_result);
+    if (api_ret==0) {
+#if 0
+        {
+            char tmp[128];
+            int i;
+            char *p = tmp;
+            for (i=0; i<ZMOD4510_ADC_DATA_LEN;i++) {
+                p += tiny_sprintf(p, "%02x ", zmod->adc_result[i]);
+            }
+            BSP_TRACE("Sensor: %s", tmp);
+        }
+#endif
+        return ZMOD4510_OP_SUCCESS;
+    }
+    return ZMOD4510_OP_DEVICE_ERROR;
+}
+
+const uint8_t *ZMOD4510_get_adc(ZMOD4510_t *zmod)
+{
+    return zmod->adc_result;
+}
+
+ZMOD4510_op_result_t ZMOD4510_is_sequencer_completed(ZMOD4510_t *zmod)
+{
+    uint8_t zmod4xxx_status;
+
+    int8_t api_ret = zmod4xxx_read_status(&zmod->dev, &zmod4xxx_status);
+    if (api_ret!=0) {
+        return ZMOD4510_OP_DEVICE_ERROR;
+    }
+
+    if ((zmod4xxx_status & STATUS_SEQUENCER_RUNNING_MASK)) {
+        return ZMOD4510_OP_BUSY;
+    }
+
+    return ZMOD4510_OP_SUCCESS;
+
+}
+
 
---
+++ uair/Software/lib/ZMOD4510/ZMOD4510_OAQ2.c
@@ -0,0 +1,45 @@
+#include "ZMOD4510_OAQ2.h"
+#include "zmod4510_config_oaq2.h"
+#include "oaq_2nd_gen.h"
+#include "BSP.h"
+
+ZMOD4510_OAQ2_error_t ZMOD4510_OAQ2_init(ZMOD4510_OAQ2_t *oaq, zmod4xxx_dev_t *dev)
+{
+    int8_t lib_ret = init_oaq_2nd_gen(&oaq->algo_handle, dev);
+    if (lib_ret!=0) {
+        BSP_TRACE("Cannot init OAQ library");
+        return ZMOD4510_OAQ2_ERROR_UNSPECIFIED;
+    }
+    oaq->dev = dev;
+    return ZMOD4510_OAQ2_NO_ERROR;
+}
+
+
+ZMOD4510_OAQ2_error_t ZMOD4510_OAQ2_calculate(ZMOD4510_OAQ2_t *oaq,
+                               const uint8_t *adc_result,
+                               const float humidity_pct,
+                               const float temperature_degc,
+                               oaq_2nd_gen_results_t *results)
+{
+    ZMOD4510_OAQ2_error_t err;
+    int8_t lib_ret = calc_oaq_2nd_gen(&oaq->algo_handle,
+                                      oaq->dev,
+                                      adc_result,
+                                      humidity_pct,
+                                      temperature_degc,
+                                      results);
+
+    switch (lib_ret) {
+    case OAQ_2ND_GEN_STABILIZATION:
+        err = ZMOD4510_OAQ2_ERROR_STABILIZING;
+        break;
+    case OAQ_2ND_GEN_OK:
+        err = ZMOD4510_OAQ2_NO_ERROR;
+        break;
+    default:
+        BSP_TRACE("OAQ error: %d", lib_ret);
+        err = ZMOD4510_OAQ2_ERROR_UNSPECIFIED;
+        break;
+    }
+    return err;
+}
