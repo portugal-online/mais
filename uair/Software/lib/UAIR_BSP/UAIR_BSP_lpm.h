e708ffcb267f66cb7c6ee48d22fc95459987b9e7
Alvaro <alvieboy@alvie.com>
2021-11-20 21:35:38 +0000

    lpm: add debug pin

--- uair/Software/lib/UAIR_BSP/UAIR_BSP_lpm.h
+++ uair/Software/lib/UAIR_BSP/UAIR_BSP_lpm.h
@@ -1,6 +1,9 @@
 #ifndef __UAIR_BSP_LPM_H__
 #define __UAIR_BSP_LPM_H__
 
+/**
+ * Enter Low power mode. This mode is either STOP2 or STOP1, depending on
+ */
 void BSP_LPM_enter_low_power_mode(void);
 
 #endif
