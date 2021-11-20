b4054a6183c391122d54e4ed054461d19c073701
Alvaro <alvieboy@alvie.com>
2021-11-20 21:33:07 +0000

    gpio: add more methods

--- uair/Software/lib/UAIR_HAL/HAL_gpio.c
+++ uair/Software/lib/UAIR_HAL/HAL_gpio.c
@@ -26,6 +26,19 @@ HAL_StatusTypeDef HAL_GPIO_configure_output_od(const HAL_GPIODef_t *gpio)
     return HAL_OK;
 }
 
+HAL_StatusTypeDef HAL_GPIO_configure_input(const HAL_GPIODef_t *gpio)
+{
+    GPIO_InitTypeDef gpio_init_structure = {0};
+
+    gpio_init_structure.Pin = gpio->pin;
+    gpio_init_structure.Mode = GPIO_MODE_INPUT;
+    gpio_init_structure.Pull = GPIO_NOPULL;
+    gpio_init_structure.Speed = GPIO_SPEED_FREQ_LOW;
+
+    HAL_GPIO_Init(gpio->port, &gpio_init_structure);
+    return HAL_OK;
+}
+
 HAL_StatusTypeDef HAL_GPIO_set(const HAL_GPIODef_t *gpio, int value)
 {
     HAL_GPIO_WritePin(gpio->port, gpio->pin, value);
