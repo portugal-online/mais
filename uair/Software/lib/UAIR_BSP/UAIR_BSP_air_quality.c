d77b038f022c51169c55d5ca4a91ddba6aab31eb
Alvaro <alvieboy@alvie.com>
2021-12-01 07:06:45 +0000

    BSP: air quality resiliency

--- uair/Software/lib/UAIR_BSP/UAIR_BSP_air_quality.c
+++ uair/Software/lib/UAIR_BSP/UAIR_BSP_air_quality.c
@@ -9,6 +9,8 @@
 static ZMOD4510_t zmod;
 static ZMOD4510_OAQ2_t zmod_oaq;
 static BSP_sensor_state_t sensor_state = SENSOR_OFFLINE;
+static unsigned sampleno = 0;
+static BSP_I2C_busnumber_t i2c_busno;
 
 static HAL_GPIODef_t reset_gpio = {
     .port = GPIOC,
@@ -20,7 +22,24 @@ static HAL_GPIODef_t reset_gpio = {
 
 BSP_error_t UAIR_BSP_air_quality_init()
 {
-    HAL_I2C_bus_t bus = UAIR_BSP_I2C_GetHALHandle(BSP_I2C_BUS0);
+    BSP_powerzone_t powerzone = UAIR_POWERZONE_NONE;
+
+    switch (BSP_get_board_version()) {
+    case UAIR_NUCLEO_REV1:
+        i2c_busno = BSP_I2C_BUS0;
+        powerzone = UAIR_POWERZONE_AMBIENTSENS;
+        break;
+    case UAIR_NUCLEO_REV2:
+        i2c_busno = BSP_I2C_BUS2;
+        powerzone = UAIR_POWERZONE_AMBIENTSENS;
+        break;
+    default:
+        break;
+    }
+    if (powerzone==UAIR_POWERZONE_NONE)
+        return BSP_ERROR_NO_INIT;
+
+    HAL_I2C_bus_t bus = UAIR_BSP_I2C_GetHALHandle(i2c_busno);
 
     BSP_error_t err = ZMOD4510_Init(&zmod, bus, &reset_gpio);
     if (err!=BSP_ERROR_NONE)
@@ -38,13 +57,6 @@ BSP_error_t UAIR_BSP_air_quality_init()
     return err;
 }
 
-BSP_error_t BSP_air_quality_start_measurement()
-{
-    if (ZMOD4510_start_measurement(&zmod)==ZMOD4510_OP_SUCCESS)
-        return BSP_ERROR_NONE;
-    return BSP_ERROR_COMPONENT_FAILURE;
-}
-
 BSP_error_t BSP_air_quality_measurement_completed()
 {
     ZMOD4510_op_result_t r = ZMOD4510_is_sequencer_completed(&zmod);
@@ -54,7 +66,53 @@ BSP_error_t BSP_air_quality_measurement_completed()
     return BSP_ERROR_NONE;
 }
 
-static unsigned sampleno = 0;
+static BSP_error_t UAIR_BSP_air_quality_zmod_op(ZMOD4510_op_result_t (*op)(ZMOD4510_t *zmod))
+{
+    ZMOD4510_op_result_t r;
+    BSP_I2C_recover_action_t action;
+    int retries=1;
+    BSP_error_t err = BSP_ERROR_NONE;
+
+    do {
+        r = op(&zmod);
+        switch (r) {
+        case ZMOD4510_OP_DEVICE_ERROR:
+            action = UAIR_BSP_I2C_analyse_and_recover_error(i2c_busno);
+            BSP_TRACE("Recover action: %d", action);
+            err = BSP_ERROR_COMPONENT_FAILURE;
+            break;
+        case ZMOD4510_OP_BUSY:
+            err = BSP_ERROR_BUSY;
+            break;
+        case ZMOD4510_OP_SUCCESS:
+            err = BSP_ERROR_NONE;
+            break;
+        default:
+            err = BSP_ERROR_COMPONENT_FAILURE;
+            break;
+        }
+    } while ((err!=BSP_ERROR_NONE) && (retries--));
+
+    return err;
+}
+
+static BSP_error_t UAIR_BSP_air_quality_read_adc(void)
+{
+    return UAIR_BSP_air_quality_zmod_op( &ZMOD4510_read_adc );
+}
+
+BSP_error_t BSP_air_quality_start_measurement()
+{
+    return UAIR_BSP_air_quality_zmod_op( &ZMOD4510_start_measurement );
+}
+
+
+static BSP_error_t UAIR_BSP_air_quality_sequencer_completed(void)
+{
+    return UAIR_BSP_air_quality_zmod_op( &ZMOD4510_is_sequencer_completed );
+}
+
+
 
 BSP_error_t BSP_air_quality_calculate(const float temp_c,
                                       const float hum_pct,
@@ -63,10 +121,17 @@ BSP_error_t BSP_air_quality_calculate(const float temp_c,
     oaq_2nd_gen_results_t libresult;
     BSP_error_t err;
 
-    ZMOD4510_op_result_t r = ZMOD4510_read_adc(&zmod);
+    err = UAIR_BSP_air_quality_sequencer_completed();
 
-    if (r!=ZMOD4510_OP_SUCCESS)
-        return BSP_ERROR_COMPONENT_FAILURE;
+    if (err != BSP_ERROR_NONE)
+        return err;
+
+
+    err = UAIR_BSP_air_quality_read_adc();
+
+    if (err != BSP_ERROR_NONE)
+        return err;
+
     BSP_TRACE("Aquired sample no %d (901 needed for cal.)", sampleno);
     sampleno++;
 
