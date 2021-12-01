487532b39ff466ddd63c102e08925f34a4a0bae8
Alvaro <alvieboy@alvie.com>
2021-12-01 07:07:31 +0000

    BSP LPM: reinstate I2C registers upon wakeup

--- uair/Software/lib/UAIR_BSP/UAIR_BSP_lpm.c
+++ uair/Software/lib/UAIR_BSP/UAIR_BSP_lpm.c
@@ -8,13 +8,22 @@ void UAIR_LPM_PostStopModeHook()
 {
     HAL_clk_resume_clocks();
 
+    // I2C2, I2C3 lose register contents upon STOP2 mode.
+    BSP_error_t err = UAIR_BSP_I2C_exit_low_power_mode();
+
     TRACER_RESUME();
+
     UAIR_BSP_DP_Off(DEBUG_PIN2);
 
+    if (err) {
+        BSP_TRACE("While re-initialising I2C buses: error %d", err);
+    }
 }
 
 void BSP_LPM_enter_low_power_mode()
 {
+    UAIR_BSP_I2C_enter_low_power_mode();
+
     UAIR_BSP_DP_On(DEBUG_PIN2);
     UAIR_LPM_EnterLowPower();
 }
