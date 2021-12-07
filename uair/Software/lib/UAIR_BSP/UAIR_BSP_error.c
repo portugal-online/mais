ba1198df84b2768e59081916cf75a92a9374c6c3
Alvaro <alvieboy@alvie.com>
2021-12-07 08:27:44 +0000

    BSP: implement error retrieval

--- uair/Software/lib/UAIR_BSP/UAIR_BSP_error.c
+++ uair/Software/lib/UAIR_BSP/UAIR_BSP_error.c
@@ -7,5 +7,9 @@ BSP_error_detail_t bsp_error_lasterror;
 void BSP_error_push(BSP_error_detail_t error)
 {
     bsp_error_lasterror = error;
-    BSP_STOP_FOR_POWER_CALCULATION();
+}
+
+BSP_error_detail_t BSP_error_get_last_error(void)
+{
+    return bsp_error_lasterror;
 }
