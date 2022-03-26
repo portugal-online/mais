e2dcb6ba1df53cbe6d06c2e8d21ea8a46e86863e
Alvaro <alvieboy@alvie.com>
2022-03-26 08:54:55 +0000

    hostmode: add flash suppot

---
+++ uair/hostmode/target/stm32wlxx_hal_driver/Src/flash_support.s
@@ -0,0 +1,17 @@
+.data
+.globl _rom_start
+.globl _rom_end
+.globl _flash_end
+.globl config_storage
+.globl log_storage
+
+       .align 8
+_rom_start:
+       .space 8192,0
+config_storage: /* Two 2K pages */
+       .space 4096,0
+log_storage: /* Fourteen 2K pages */
+       .space 28672,0
+_rom_end:
+       .space 8192,0
+_flash_end:
