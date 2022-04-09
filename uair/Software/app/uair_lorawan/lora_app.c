36f438131f21b02f57f89733ad9db464411b8f86
Nuno Ferreira <self@nunof.eu>
2022-04-09 12:16:06 +0100

    code cleanups and enabled led on lora tx

--- uair/Software/app/uair_lorawan/lora_app.c
+++ uair/Software/app/uair_lorawan/lora_app.c
@@ -73,7 +73,7 @@ typedef enum TxEventType_e
   * @param  LED context
   * @return none
   */
-//static void OnTimerLedEvent(void *context);
+static void OnTimerLedEvent(void *context);
 
 /**
   * @brief  join event callback function
@@ -154,7 +154,7 @@ static LmHandlerParams_t LmHandlerParams =
 /**
   * @brief Timer to handle the application Tx Led to toggle
   */
-//static UTIL_TIMER_Object_t TxLedTimer;
+static UTIL_TIMER_Object_t TxLedTimer;
 
 void LoRaWAN_Init(UAIR_link_commands_t *cmd_callbacks)
 {
@@ -177,15 +177,7 @@ void LoRaWAN_Init(UAIR_link_commands_t *cmd_callbacks)
 #endif
 
   UAIR_cmd_callbacks = cmd_callbacks;
-/*
-  if (EventType == TX_ON_TIMER)
-  {
-    // send every time timer elapses
-    UTIL_TIMER_Create(&TxTimer, 0xFFFFFFFFU, UTIL_TIMER_ONESHOT, OnTxTimerEvent, NULL);
-    UTIL_TIMER_SetPeriod(&TxTimer, SENSORS_TX_DUTYCYCLE);
-    UTIL_TIMER_Start(&TxTimer);
-  }
-*/
+
 }
 
 static void OnRxData(LmHandlerAppData_t *appData, LmHandlerRxParams_t *params)
@@ -234,50 +226,6 @@ static void OnRxData(LmHandlerAppData_t *appData, LmHandlerRxParams_t *params)
     }
   }
 }
-/*
-static uint8_t EncodeTemperature(uint32_t temp_millicentigrades)
-{
-    
-     Encodes a temperature between -11.75C and +52C into 8-bit, with 0.25C accuracy
-
-     To extract the temperature from the data (B), use:
-
-      T = (B-47)/4
-
-     */
-/*
-    int32_t rtemp = (temp_millicentigrades/250) + 47;
-    if (rtemp<0)
-        rtemp = 0;
-    if (rtemp>255)
-        rtemp = 255;
-    return (uint8_t)(rtemp & 0xff);
-}
-*/
-
-/*
-static uint8_t EncodeHumidity(uint32_t hum_millipercent)
-{
-    
-     Encodes a humidity (0-100%) into 8-bit, using 0.5% accuracy
-
-     To extract the humidity from the data (B), use:
-
-      H= B/2
-
-     */
-/*    
-    int32_t rhum = hum_millipercent / 500;
-
-    if (rhum<0)
-        rhum=0;
-
-    if (rhum>200)
-        rhum=200;
-
-    return (int8_t)(rhum & 0xff);
-}
-*/
 
 uint8_t UAIR_lora_send(uint8_t buf[], uint8_t len) {
   UTIL_TIMER_Time_t nextTxIn = 0;
@@ -285,6 +233,12 @@ uint8_t UAIR_lora_send(uint8_t buf[], uint8_t len) {
   //AppData.BufferSize = sizeof(*buf)/sizeof(uint8_t);
   AppData.BufferSize = len;
   AppData.Buffer = buf;
+  UTIL_TIMER_Create(&TxLedTimer, 0xFFFFFFFFU, UTIL_TIMER_ONESHOT, OnTimerLedEvent, NULL);
+  UTIL_TIMER_SetPeriod(&TxLedTimer, 200);
+  UTIL_TIMER_Start(&TxLedTimer);
+
+  BSP_LED_on(LED_BLUE);
+
   LmHandlerErrorStatus_t res = LmHandlerSend(&AppData, LORAWAN_DEFAULT_CONFIRMED_MSG_STATE, &nextTxIn, false);
   APP_LOG(ADV_TRACER_TS_ON, ADV_TRACER_VLEVEL_L, "SEND REQUEST %d\r\n", res);
 
@@ -332,22 +286,11 @@ static void SendTxData(void)
 }
 */
 
-/*
-static void OnTxTimerEvent(void *context)
-{
-    UTIL_SEQ_SetTask((1 << CFG_SEQ_Task_LoRaSendOnTxTimerOrButtonEvent), CFG_SEQ_Prio_0);
-
-    // Wait for next tx slot
-    UTIL_TIMER_Start(&TxTimer);
-}
-*/
-
-/*
 static void OnTimerLedEvent(void *context)
 {
     BSP_LED_off(LED_BLUE);
 }
-*/
+
 
 static void OnTxData(LmHandlerTxParams_t *params)
 {
