48b35b9926cd0f4465d287e3a7fef09e7bc83b2b
Alvaro <alvieboy@alvie.com>
2022-04-26 21:01:20 +0100

    Move payload

--- uair/Software/app/uair_lorawan/lora_app.c
+++ uair/Software/app/uair_lorawan/lora_app.c
@@ -48,43 +48,7 @@ struct generic_payload
 */
 
 #ifdef DEBUGGER_ON
-struct payload_type0
-{
-    uint8_t max_oaq_msb:1;
-    uint8_t epa_oaq_msb:1;
-    uint8_t health_oaq:1;
-    uint8_t health_microphone:1;
-    uint8_t health_ext_temp_hum:1;
-    uint8_t health_int_temp_hum:1;    
-    uint8_t payload_type:2;
-
-    uint8_t avg_ext_temp;
-
-    uint8_t max_sound_level_msb:1;
-    uint8_t avg_ext_hum:7;
-
-    uint8_t epa_oaq_lsb;
-
-    uint8_t max_oaq_lsb;
-
-    uint8_t avg_sound_level_lsb:4;
-    uint8_t max_sound_level_lsb:4;
-
-    uint8_t max_int_temp;
-
-    uint8_t avg_sound_level_msb:1;
-    uint8_t max_int_hum:7;
-} __attribute__((packed));
-
-/*
-struct payload_type1
-{
-    struct payload_type0 p0;
-    uint8_t battery_level:7;
-    uint8_t system_restart:1;
-    uint8_t errors_logged;
-} __attribute__((packed));
-*/
+#include "uair_payloads.h"
 #endif
 
 
