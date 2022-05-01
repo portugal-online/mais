da73b50f22e22a330ee5bba4066d8ef7a84be1cc
João Ramos <joao.sigma@gmail.com>
2022-05-01 17:23:29 +0100

    Updated documentation and catch2 test flags.

--- uair/Software/app/uair_lorawan/io/UAIR_config_api.h
+++ uair/Software/app/uair_lorawan/io/UAIR_config_api.h
@@ -7,24 +7,89 @@
 extern "C" {
 #endif
 
+/**
+ * The different config ids / keys that can be used in the API.
+ * 
+ * For each item, a corresponding entry in "uair_io_context_keys" should exist.
+ */
 typedef enum {
     UAIR_CONFIG_ID_TX_POLICY = UAIR_IO_CONTEXT_KEY_CONFIG_TX_POLICY,
     UAIR_CONFIG_ID_FAIR_RATIO = UAIR_IO_CONTEXT_KEY_CONFIG_FAIR_RATIO
 } uair_config_id;
 
+
+/**
+ * A pair consisting of a config id and a unsigned int8 value.
+ */ 
 typedef struct {
    uair_config_id id;
    uint8_t value;
 } uair_config_pair_uint8;
 
+/**
+ * Sets and returns the cache size.
+ * 
+ * If \p new_size is negative, then the size isn't changed and it simply returns
+ * the current size. If 0, then it disables the cache. Otherwise, it
+ * updates the cache size (a positive \p new_size, even if equal to the current
+ * value, always resets the cache).
+ * 
+ * @param new_size the new size the cache should take.
+ * @return the actual value of the cache
+ */ 
 int8_t uair_config_cache_size(int8_t new_size);
 
+/**
+ * Sets or updates the value of a config of type unsigned int8.
+ * 
+ * @param id the target config id
+ * @param value the new value of the config
+ * @return 0 if successful, otherwise the action failed (the value
+ * matches with the values defined in "uair_io_context_errors")
+ */
 int uair_config_write_uint8(uair_config_id id, uint8_t value);
+/**
+ * Retrieves the value of a config of type unsigned int8.
+ * 
+ * The param \p value can be NULL, in which case the method simply checks
+ * if the key is present and if it's of the correct type.
+ * 
+ * @param id the target config id
+ * @param value where to store the value of the config
+ * @return 0 if successful, otherwise the action failed (the value
+ * matches with the values defined in "uair_io_context_errors")
+ */
 int uair_config_read_uint8(uair_config_id id, uint8_t *value);
 
+/**
+ * Returns the defaults values for certain config ids (not necessarily
+ * all of them).
+ * 
+ * @param id if not NULL, stores the number of pairs returned
+ * @return A list of pairs of config id / values
+ */
 const uair_config_pair_uint8* config_defaults_uint8(int* size);
 
+/**
+ * Sets or updates the value of multiple configs of type unsigned int8.
+ * 
+ * @param pairs the pairs of config id / value to write
+ * @param size the number of pairs in \p pairs
+ * @return 0 if successful, otherwise the action failed (the value
+ * matches with the values defined in "uair_io_context_errors")
+ */
 int uair_config_write_uint8s(const uair_config_pair_uint8* pairs, int size);
+/**
+ * Retrieves the values of multiple configs of type unsigned int8.
+ * 
+ * The value for each entry is written into the pair. Any value stored is ignored
+ * and overwritten.
+ * 
+ * @param pairs the pairs of config id to read
+ * @param size the number of pairs in \p pairs
+ * @return 0 if successful, otherwise the action failed (the value
+ * matches with the values defined in "uair_io_context_errors")
+ */
 int uair_config_read_uint8s(uair_config_pair_uint8* pairs, int size);
 
 #ifdef __cplusplus
--- uair/Software/app/uair_lorawan/io/UAIR_io_base.h
+++ uair/Software/app/uair_lorawan/io/UAIR_io_base.h
@@ -8,31 +8,31 @@
 extern "C" {
 #endif
 
-/* UAIR IO context flags */
+/** UAIR IO context flags */
 typedef enum {
-    /* No action is required */
+    /** No action is required */
     UAIR_IO_CONTEXT_FLAG_NONE = 0,
-    /* Flush and try again */
+    /** Flush and try again */
     UAIR_IO_CONTEXT_FLAG_FLUSH = (1 << 0)
 } uair_io_context_flags;
 
-/* UAIR IO context errors */
+/** UAIR IO context errors */
 typedef enum {
-    /* No error present (last action was successful) */
+    /** No error present (last action was successful) */
     UAIR_IO_CONTEXT_ERROR_NONE = 0,
-    /* Error: no action was performed because context was in an invalid state */
+    /** Error: no action was performed because context was in an invalid state */
     UAIR_IO_CONTEXT_ERROR_CTX_INVALID = 1,
-    /* Error: action was canceled but can be retried (check flags on how to proceed) */
+    /** Error: action was canceled but can be retried (check flags on how to proceed) */
     UAIR_IO_CONTEXT_ERROR_CTX_CHECK = 2,
-    /* Error: the flush command couldn't run because there's no free page to use as buffer */
+    /** Error: the flush command couldn't run because there's no free page to use as buffer */
     UAIR_IO_CONTEXT_ERROR_FLUSH_NO_FREE_PAGE = 3,
-    /* Error: the flush command couldn't run because there's no free page to use as buffer */
+    /** Error: the flush command couldn't run because there's no free page to use as buffer */
     UAIR_IO_CONTEXT_ERROR_NO_SPACE_AVAILABLE = 4,
-    /* Error: error while trying to write */
+    /** Error: error while trying to write */
     UAIR_IO_CONTEXT_ERROR_WRITE = 5,
-    /* Error: error while trying to write */
+    /** Error: error while trying to write */
     UAIR_IO_CONTEXT_ERROR_READ = 6,
-    /* Error: an internal error occurred */
+    /** Error: an internal error occurred */
     UAIR_IO_CONTEXT_ERROR_INTERNAL = 7,
 
     UAIR_IO_CONTEXT_ERROR_EXT_BASE = 10
@@ -43,7 +43,6 @@ typedef enum {
  *
  * Call UAIR_io_init_ctx() before first use.
  * Do *not* change this values directly, they're just for reading.
- * 
  */
 typedef struct {
     uair_io_context_flags flags;
@@ -53,7 +52,9 @@ typedef struct {
 /**
  * Initializes an io_context struct.
  * 
- * All existing data is ignored and removed.
+ * All existing data in the context is ignored and removed.
+ * 
+ * @param cyx Pointer to the context to initialize.
  */
 void UAIR_io_init_ctx(uair_io_context* ctx);
 
