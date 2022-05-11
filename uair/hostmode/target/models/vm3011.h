cb86dcb9f0a8c65eca49cbea54d9f469caa4ef84
Alvaro <alvieboy@alvie.com>
2022-05-11 11:29:46 +0100

    vm3011: add C++ export

--- uair/hostmode/target/models/vm3011.h
+++ uair/hostmode/target/models/vm3011.h
@@ -1,7 +1,14 @@
+#ifndef MODEL_VM3111_H__
+#define MODEL_VM3111_H__
+
 #include "stm32wlxx_hal.h"
 #include "stm32wlxx_hal_i2c_pvt.h"
 
 // Model for VM3011
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 struct vm3011_model;
 
@@ -15,4 +22,8 @@ void vm3011_set_read_callback(struct vm3011_model *, void (*callback)(void *user
 extern struct i2c_device_ops vm3011_ops;
 
 
+#ifdef __cplusplus
+}
+#endif
 
+#endif
